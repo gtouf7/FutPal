6daddc248e71cfd5e32bf78f60027f880575473a
Georgios Toufexis <georgetouf7@gmail.com>
2024-09-24 02:01:13 -0400

    Added favicon

--- frontend-next/src/app/layout.js
+++ frontend-next/src/app/layout.js
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/futpal-logo.png"/>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
