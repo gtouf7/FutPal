4262416388165bbba20c38a5e5d62bb6ca813f0c
Georgios Toufexis <georgetouf7@gmail.com>
2024-10-01 15:03:56 -0400

    Added team assign frontend logic

--- frontend-next/src/app/teamAssign/page.js
+++ frontend-next/src/app/teamAssign/page.js
@@ -2,6 +2,7 @@
 'use client';
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { set } from "mongoose";
 
 export default function teamAssign() {
     // Check if user is logged in
@@ -13,6 +14,35 @@ export default function teamAssign() {
                 router.push('/login'); // If there is no token this page will be inaccessible and will redirect the user to the login page
             }
         }, [router]);
+    }
+    userAuth();
+
+    const [ teamId, setTeamId ] = useState('');
+    const [ message, setMessage ] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('/api/assignTeam', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    Authorizaion: `Bearer ${localStorage.getItem('token')}`,
+                },
+                body: JSON.stringify({ teamId }),
+            });
+
+            const data = response.json();
+            if (response.ok) {
+                setMessage('Team successfully assigned');
+            } else {
+                setMessage(data.message || 'Error assigning team.');
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+            setMessage('Something went wrong');
+        }
     }
     return(
         <>
